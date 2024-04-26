23685b109adf9e40d84556d7a93b27e8334fb930
Manirabona Patience <hseal419@gmail.com>
2024-04-26 16:39:51 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import passport from 'passport';
+import session from 'express-session';
+
+dotenv.config();
+
+const app = express();
+const app_port = process.env.APP_URL_PORT;
+
+app.use(session({ secret: process.env.AUTH_SECRET_KEY, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(app_port, () => console.log(`App is listening to port: ${app_port}`));
